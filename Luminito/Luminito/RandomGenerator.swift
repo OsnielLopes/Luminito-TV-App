4a7096f23ed6795026985428cfb3f257a1b09771
Pedro Ortiz <p.delrioortiz1997@gmail.com>
2018-05-29 18:35:48 -0300

    Stars Generator

---
+++ Luminito/Luminito/RandomGenerator.swift
@@ -0,0 +1,9 @@
+//
+//  RandomGenerator.swift
+//  Luminito
+//
+//  Created by Pedro Del Rio Ortiz on 29/05/2018.
+//  Copyright © 2018 Osniel Lopes Teixeira. All rights reserved.
+//
+
+import Foundation
