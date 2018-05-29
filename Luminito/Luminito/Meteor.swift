61f4e755fdd04797a8ec3817ae0b9fbc808aebd8
Vinicius Cano <viniciuscano.vc@gmail.com>
2018-05-29 17:35:02 -0300

    Criação de classe Meteor

---
+++ Luminito/Luminito/Meteor.swift
@@ -0,0 +1,13 @@
+//
+//  Meteor.swift
+//  Luminito
+//
+//  Created by Vinícius Cano Santos on 29/05/2018.
+//  Copyright © 2018 Osniel Lopes Teixeira. All rights reserved.
+//
+
+import UIKit
+
+class Meteor: SKSpriteNode {
+
+}
