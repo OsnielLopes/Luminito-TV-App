421a9a4a92c158ed8f06c17c29cdc4c61580bd0e
Pedro Ortiz <p.delrioortiz1997@gmail.com>
2018-06-02 18:30:57 -0300

    inicio implementacao HUD e velocityBoost

---
+++ Luminito/Luminito/PowerUpGenerator.swift
@@ -0,0 +1,9 @@
+//
+//  PowerUpGenerator.swift
+//  Luminito
+//
+//  Created by Pedro Del Rio Ortiz on 02/06/2018.
+//  Copyright © 2018 Osniel Lopes Teixeira. All rights reserved.
+//
+
+import Foundation
